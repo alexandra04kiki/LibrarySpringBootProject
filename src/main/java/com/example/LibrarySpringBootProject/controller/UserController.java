ded6dc55cc2417085346547f52280ea1c080befd
unknown <chindris.vi.al@student.utcluj.ro>
2025-01-14 19:41:24 +0200

    Configure datasource

---
+++ src/main/java/com/example/LibrarySpringBootProject/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.example.LibrarySpringBootProject.controller;
+
+import com.example.LibrarySpringBootProject.dto.UserDto;
+import com.example.LibrarySpringBootProject.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/users")
+    public String getUsers(Model model){
+        List<UserDto> userDtos = userService.getAllUsers();
+        model.addAttribute("title", "Users");
+        model.addAttribute("users", userDtos);
+        return "users";
+    }
+
+    @GetMapping("/users/{id}")
+    public UserDto getUserById(@PathVariable Integer id){
+        return userService.getUserById(id);
+    }
+}
+
---
+++ src/main/java/com/example/LibrarySpringBootProject/service/UserService.java
@@ -0,0 +1,27 @@
+package com.example.LibrarySpringBootProject.service;
+
+
+import com.example.LibrarySpringBootProject.dto.UserDto;
+import com.example.LibrarySpringBootProject.model.RegistrationRequest;
+import com.example.LibrarySpringBootProject.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    boolean checkEmail(String email);
+
+    UserDto registerUser(RegistrationRequest registrationRequest);
+
+    UserDto getLoginUser();
+
+    UserDto getUserById(Integer id);
+
+    List<UserDto> getAllUsers();
+
+    UserDto createUser(User user);
+
+    UserDto updateUser(User user);
+
+    void deleteUser(User user);
+}
